3f9b2a8497b8b7ee5a8c78a92d76d9581aa9393c
kasba <kasba@neuf.fr>
2012-11-14 15:59:06 +0100

    Migration Python 3

--- paulla/ircbot/ircbot.py
+++ paulla/ircbot/ircbot.py
@@ -12,24 +12,23 @@ class Botanik(irc.bot.SingleServerIRCBot):
     def __init__(self, channel="#paulla", nickname="botanik", server="irc.freenode.net", port=6667):
         irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
         self.channel = channel
-        self.yakafokon = [u'falloir',u'faut',u'il faille',u'faudra',u'fallait',u'il fallût',u'fallu']
+        self.yakafokon = ['falloir','faut','il faille','faudra','fallait','il fallût','fallu']
 
     def on_welcome(self, c, e):
         """ Le bot dit bonjour """
         c.join(self.channel)
-        c.privmsg(self.channel, u"cyp c'est pour quand le prochain sprint ?")
-        c.privmsg(self.channel, u"cyp j'ai vraiment l'impression de mouler !")
+        c.privmsg(self.channel, "cyp c'est pour quand le prochain sprint ?")
+        c.privmsg(self.channel, "cyp j'ai vraiment l'impression de mouler !")
         lost_masters = ['solevis', 'Llew', 'Mika64']
         for master in lost_masters:
-            c.privmsg(self.channel, u"Mon maître %s m'a abandonné" % master)
-        c.privmsg(self.channel, u"Mon maître Kasba m'a trouvé")
+            c.privmsg(self.channel, "Mon maître %s m'a abandonné" % master)
         
     def on_pubmsg(self, c, e):
         """ Le bot écoute ce qui se raconte """
         message = e.arguments()[0]
         for terme in self.yakafokon:
             if terme in message:
-                c.privmsg(self.channel, u"¡¡¡ YAKAFOKON detected !!!")
+                c.privmsg(self.channel, "¡¡¡ YAKAFOKON detected !!!")
                 break
      
 
